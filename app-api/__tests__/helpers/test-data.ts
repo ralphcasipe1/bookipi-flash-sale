fb92dda74f3c11301d3395ea4607a938bfe7c430
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2026-06-09 22:01:34 +0800

    :clown_face: update fn for mocking tests to support special characters on email

--- app-api/__tests__/helpers/test-data.ts
+++ app-api/__tests__/helpers/test-data.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-export const randomUserId = (): string => faker.internet.email();
+export const randomUserId = (): string => faker.internet.email({ allowSpecialCharacters: true });
 
 export const randomUserIds = (count: number): string[] =>
   Array.from({ length: count }, () => randomUserId());
